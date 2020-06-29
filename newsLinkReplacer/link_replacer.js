511b8423623614bf3cfd6e6a06f0d0b82f8f02c6
Luk3-S <lstevenson63@gmail.com>
2020-06-29 18:00:49 +0100

    SO links on test page now change to link to reddit

--- newsLinkReplacer/link_replacer.js
+++ newsLinkReplacer/link_replacer.js
@@ -1,4 +1,17 @@
-$("a").attr("href", "https://www.w3schools.com/");
+//$("a[href^='http://stackoverflow.com']").attr("href", "https://www.w3schools.com/");
 
-$("a").text("MySQL Tutorial");
+// $("a[href^='https://stackoverflow.com']").each(function(){
+//     this.href = this.href.replace('href', 
+//         "https://www.youtube.com/watch?v=660L1AmOh2g")
+// });
+
+
+
+
+var matched_tags = document.querySelectorAll('a[href^="https://stackoverflow.com"]');
+
+matched_tags.forEach(function(tag){
+    console.log("take a break");
+    tag.setAttribute('href','https://www.reddit.com/')
+});
 
