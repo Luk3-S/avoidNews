ee934d4b9ea6e1dd71aa25a088591f88c2ef218b
Luk3-S <lukesp63@gmail.com>
2020-10-22 13:13:38 +0100

    Issue 10 resolved, can add and subtract from list of sites

--- newsLinkReplacer/options.js
+++ newsLinkReplacer/options.js
@@ -1,25 +1,69 @@
-function getUrls() {
+function getUrls(arg) {
+    console.log(arg)
     chrome.storage.sync.get({
         sites_to_replace: []
     }, function (data) {
-        console.log(data.sites_to_replace);
-        update(data.sites_to_replace);
+        //console.log(data.sites_to_replace);
+
+        if (arg == "add") {
+            update(data.sites_to_replace);
+        } else {
+            remove(data.sites_to_replace);
+        }
     });
 }
 
 function update(url_list) {
     var url = document.getElementById('textInputAdd').value;
-    url_list.push(url);
+    var saved = document.getElementById('saved');
+    const index = url_list.indexOf(url);
+    var present = true;
+    if (index == -1) {
+        present = false;
+        url_list.push(url);
+    }
     chrome.storage.sync.set({
         sites_to_replace: url_list
     }, function () {
+        if (!present) {
+            saved.textContent = url + " has been added to list of avoided sites.";
+        } else {
+            saved.textContent = url + " is already on the list of avoided sites.";
 
-        var saved = document.getElementById('saved');
-        saved.textContent = url + " has been added to list of avoided sites.";
+        }
         setTimeout(function () {
             saved.textContent = '';
+            document.getElementById('textInputAdd').value = "";
         }, 1500);
     });
 }
 
-document.getElementById('saveAddition').addEventListener('click', getUrls); //save_options);
+function remove(url_list) {
+
+    var url = document.getElementById('textInputRemove').value;
+    console.log("url:" + url + ", url list: " + url_list)
+    try {
+        const index = url_list.indexOf(url);
+        console.log("index is:" + index)
+        if (index > -1) {
+            url_list.splice(index, 1);
+        }
+    } catch {
+        console.log("Element to be removed not found in site list")
+    }
+    console.log("url_list after " + url_list)
+    chrome.storage.sync.set({
+        sites_to_replace: url_list
+    }, function () {
+
+        var removed = document.getElementById('removed');
+        removed.textContent = url + " has been removed from list of avoided sites.";
+        setTimeout(function () {
+            removed.textContent = '';
+            document.getElementById('textInputRemove').value = "";
+
+        }, 1500);
+    });
+}
+document.getElementById('saveAddition').addEventListener('click', getUrls.bind(null, "add")); //getUrls("add")); 
+document.getElementById('saveRemoval').addEventListener('click', getUrls.bind(null, "remove"));
