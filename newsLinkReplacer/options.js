78bc854eb5af60d05d4b204c7b18a40cbed70bc9
Luk3-S <lukesp63@gmail.com>
2020-10-22 16:50:47 +0100

    Refactored options.js

--- newsLinkReplacer/options.js
+++ newsLinkReplacer/options.js
@@ -3,67 +3,49 @@ function getUrls(arg) {
     chrome.storage.sync.get({
         sites_to_replace: []
     }, function (data) {
-        //console.log(data.sites_to_replace);
-
         if (arg == "add") {
-            update(data.sites_to_replace);
+            update(data.sites_to_replace, "Add");
         } else {
-            remove(data.sites_to_replace);
+            update(data.sites_to_replace, "Remove");
         }
     });
 }
 
-function update(url_list) {
-    var url = document.getElementById('textInputAdd').value;
-    var saved = document.getElementById('saved');
+function update(url_list, option) {
+    var elId = "textInput" + option;
+    var url = document.getElementById(elId).value;
+    var divTarget = document.getElementById(option);
+    var divContent = "";
     const index = url_list.indexOf(url);
-    var present = true;
-    if (index == -1) {
-        present = false;
-        url_list.push(url);
-    }
-    chrome.storage.sync.set({
-        sites_to_replace: url_list
-    }, function () {
-        if (!present) {
-            saved.textContent = url + " has been added to list of avoided sites.";
-        } else {
-            saved.textContent = url + " is already on the list of avoided sites.";
 
+    if (option == "Add") {
+        var present = true;
+        if (index == -1) {
+            present = false;
+            if (url.length > 0) {
+                url_list.push(url);
+            }
         }
-        setTimeout(function () {
-            saved.textContent = '';
-            document.getElementById('textInputAdd').value = "";
-        }, 1500);
-    });
-}
-
-function remove(url_list) {
-
-    var url = document.getElementById('textInputRemove').value;
-    console.log("url:" + url + ", url list: " + url_list)
-    try {
-        const index = url_list.indexOf(url);
-        console.log("index is:" + index)
+        divContent = present ? url + " is already on the list of avoided sites." : url + " has been added to list of avoided sites.";
+    } else {
         if (index > -1) {
             url_list.splice(index, 1);
+            divContent = url + " has been removed from list of avoided sites.";
+        } else {
+            divContent = url + "wasn't on the list of avoided sites."
         }
-    } catch {
-        console.log("Element to be removed not found in site list")
     }
-    console.log("url_list after " + url_list)
     chrome.storage.sync.set({
         sites_to_replace: url_list
     }, function () {
+        divTarget.textContent = divContent;
 
-        var removed = document.getElementById('removed');
-        removed.textContent = url + " has been removed from list of avoided sites.";
         setTimeout(function () {
-            removed.textContent = '';
-            document.getElementById('textInputRemove').value = "";
-
+            divTarget.textContent = '';
+            document.getElementById(elId).value = "";
         }, 1500);
     });
 }
-document.getElementById('saveAddition').addEventListener('click', getUrls.bind(null, "add")); //getUrls("add")); 
+
+document.getElementById('saveAddition').addEventListener('click', getUrls.bind(null, "add")); 
 document.getElementById('saveRemoval').addEventListener('click', getUrls.bind(null, "remove"));
